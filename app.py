8fbcccc8c8065e6ab28801a30f98e8dd877e08d9
felipenf47 <felipeasth@hotmail.com>
2025-03-23 22:06:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,6 @@ def read_text():
     return jsonify({"message": f"O texto recebido foi: {text}"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
